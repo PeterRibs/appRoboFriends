950b967924e01f52c81fa6a930b863fcbde22da7
PeterRibs <pedrohenriquelr@gmail.com>
2021-11-23 17:22:06 -0300

    updating

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,29 +1,4 @@
-import React from "react"; // se eu remover, dá erro, pois estamos usando JSX, que estmoas usando por conta do "HTML" que não é HTML e sim JSX
-
-// const Card = (props) => {
-//     return(
-//         <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-//             <img alt='robots' src={`https://robohash.org/${props.id}?200x200`}/>
-//             <div>
-//                 <h2>{props.name}</h2>
-//                 <p>{props.email}</p>
-//             </div>
-//         </div>
-//     );
-// }
-
-// const Card = (props) => {
-// const {name, email, id} = props
-//     return(
-//         <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-//             <img alt='robots' src={`https://robohash.org/${props.id}?200x200`}/>
-//             <div>
-//                 <h2>{props.name}</h2>
-//                 <p>{props.email}</p>
-//             </div>
-//         </div>
-//     );
-// }
+import React from "react";
 
 const Card = ({ name, email, id }) => {
     return(
